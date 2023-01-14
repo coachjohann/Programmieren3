ca2ff756fa8063bbe7ae28fc1c29bb50c4c4d94d
coachjohann <johann.muehlbach@tumo.center>
2023-01-14 11:09:42 +0100

    gol running in browser from node

--- session3/index.js
+++ session3/index.js
@@ -3,14 +3,27 @@ const express = require('express');
 
 const app = express();
 
-let message = 'mein betriebsystem ist';
+app.use(express.static("../gol"));
+
+app.get("/", function(req, res){
+    res.redirect("index.html");
+ });
+ 
 
 app.get("/name/:name", function(req, res){
     let name = req.params.name;
     res.send("<h1>Hello " + name +"</h1>");
  });
  
+ app.get("/google/:search", function(req, res){
+    let search = req.params.search;
+    res.redirect('https://www.google.com/search?q=' + search);
+ });
 
 app.listen(3000, function(){
     console.log('i started');
 });
+
+app.get("/*/", function(req, res){
+    res.status(404).send('404 not found');
+ }); 
